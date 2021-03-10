147bee6969250821649dc9f5be4caf9840b94c21
expelled-school <79050508+expelled-school@users.noreply.github.com>
2021-03-10 10:17:17 +0400

    Create picker.js

---
+++ js/picker.js
@@ -0,0 +1,46 @@
+
+document.getElementsByClassName("picker-btn")[0].onclick = function() {
+    document.getElementsByClassName("picker-data")[0].className = "picker-data picker-data-active";
+    document.getElementsByClassName("picker-data")[1].className = "picker-data";
+    document.getElementsByClassName("picker-data")[2].className = "picker-data";
+    document.getElementsByClassName("picker-data")[3].className = "picker-data";
+    document.getElementsByClassName("picker-btn")[0].className = "picker-btn picker-btn-active";
+    document.getElementsByClassName("picker-btn")[1].className = "picker-btn";
+    document.getElementsByClassName("picker-btn")[2].className = "picker-btn";
+    document.getElementsByClassName("picker-btn")[3].className = "picker-btn";
+    
+}
+
+document.getElementsByClassName("picker-btn")[1].onclick = function() {
+    document.getElementsByClassName("picker-data")[0].className = "picker-data";
+    document.getElementsByClassName("picker-data")[1].className = "picker-data picker-data-active";
+    document.getElementsByClassName("picker-data")[2].className = "picker-data";
+    document.getElementsByClassName("picker-data")[3].className = "picker-data";
+    document.getElementsByClassName("picker-btn")[0].className = "picker-btn";
+    document.getElementsByClassName("picker-btn")[1].className = "picker-btn picker-btn-active";
+    document.getElementsByClassName("picker-btn")[2].className = "picker-btn";
+    document.getElementsByClassName("picker-btn")[3].className = "picker-btn";
+    
+}
+document.getElementsByClassName("picker-btn")[2].onclick = function() {
+    document.getElementsByClassName("picker-data")[0].className = "picker-data";
+    document.getElementsByClassName("picker-data")[1].className = "picker-data";
+    document.getElementsByClassName("picker-data")[2].className = "picker-data picker-data-active";
+    document.getElementsByClassName("picker-data")[3].className = "picker-data";
+    document.getElementsByClassName("picker-btn")[0].className = "picker-btn";
+    document.getElementsByClassName("picker-btn")[1].className = "picker-btn";
+    document.getElementsByClassName("picker-btn")[2].className = "picker-btn picker-btn-active";
+    document.getElementsByClassName("picker-btn")[3].className = "picker-btn";
+    
+}
+document.getElementsByClassName("picker-btn")[3].onclick = function() {
+    document.getElementsByClassName("picker-data")[0].className = "picker-data";
+    document.getElementsByClassName("picker-data")[1].className = "picker-data";
+    document.getElementsByClassName("picker-data")[2].className = "picker-data";
+    document.getElementsByClassName("picker-data")[3].className = "picker-data picker-data-active";
+    document.getElementsByClassName("picker-btn")[0].className = "picker-btn";
+    document.getElementsByClassName("picker-btn")[1].className = "picker-btn";
+    document.getElementsByClassName("picker-btn")[2].className = "picker-btn";
+    document.getElementsByClassName("picker-btn")[3].className = "picker-btn picker-btn-active";
+    
+}
